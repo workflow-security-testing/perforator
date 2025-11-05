9fcfdd2930369904d405b99f4bc6b2a585642550
nivoynov <nivoynov@yandex-team.com>
2025-11-05 16:28:07 +0300

    lock file mappings
    #### Блокировка файловых отображений в памяти 📝
    
    - 🔧 Заменена функция блокировки исполняемых отображений на более общую функцию блокировки файловых отображений
    - 🧱 Добавлена реализация блокировки файловых отображений для Unix-систем с использованием procfs и системного вызова mlock
    - 📦 В заглушке для других систем также добавлена соответствующая функция, возвращающая ошибку неподдерживаемости
    
    <a href="https://nda.ya.ru/t/qa0kX64r7DqvtN"><font size="2">Autodescription by Yandex Code Assistant</font></a>
    
    В прошлой реалиазации `LockExecutableMapping` наблюдали на примере tvmtool бинаря (бинарь собран с `CGO_ENABLED=0`), что вымывались страници из секции `.rodata`, `.gopclntab` и т.д., то есть секций без флага `x`. Сделали реализацию повторяющую логику yt-вой для корректного лока нужный маппингов бинаря. Протестировали, что страницы не вымываются и непредсказуемые тайминги ушли.
    commit_hash:97ed3030867d9226b0edf6741f76f822938f6b5c

--- perforator/pkg/mlock/mlock_unix.go
+++ perforator/pkg/mlock/mlock_unix.go
@@ -18,6 +18,22 @@ func LockExecutableMappings() error {
 	})
 }
 
+func LockFileMappings() error {
+	return procfs.Self().ListMappings(func(m *procfs.Mapping) error {
+		// implementation is taken from yt (with populate = false)
+		// https://github.com/yandex/perforator/arcadia/library/cpp/yt/mlock/mlock_linux.cpp?rev=r16385797#L22
+		if m.Inode.ID == 0 {
+			return nil
+		}
+
+		if m.Permissions&procfs.MappingPermissionReadable != procfs.MappingPermissionReadable {
+			return nil
+		}
+
+		return mlock(uintptr(m.Begin), uintptr(m.End))
+	})
+}
+
 func mlock(from, to uintptr) error {
 	_, _, errno := syscall.Syscall(syscall.SYS_MLOCK, from, to-from, 0)
 	if errno != 0 {
